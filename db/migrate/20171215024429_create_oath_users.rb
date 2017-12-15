4fc0392194923b965bb148bba818075dac96264d
Fred Hong <fredh1993@gmail.com>
2017-12-14 21:55:30 -0500

    google oath

---
+++ db/migrate/20171215024429_create_oath_users.rb
@@ -0,0 +1,13 @@
+class CreateOathUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :oath_users do |t|
+      t.string :provider
+      t.string :uid
+      t.string :name
+      t.string :oauth_token
+      t.datetime :oauth_expires_at
+
+      t.timestamps
+    end
+  end
+end
