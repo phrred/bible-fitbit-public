93ff69a12586984e83f0a4fcf1680de2e9b1ac5d
Samuel Chiou <schiou1013@gmail.com>
2017-12-15 20:52:32 -0500

    adding new users to ministry and peer groups

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -20,16 +20,19 @@ class ProfileController < ApplicationController
 	def create
 		user =  params[:user]
 		peer_class = Group.where(group_type: "peer_class", name: user[:peer_class]).take
-		minstry = Group.where(group_type: "ministry", name: user[:ministry]).take
+		ministry = Group.where(group_type: "ministry", name: user[:ministry]).take
 		new_lifetime = Count.create(year: 0, count: 0)
 		@user = User.create!(
 			name: user[:name],
 			email: session[:email],
 			gender: user[:gender],
 			peer_class: peer_class,
-			ministry: minstry,
+			ministry: ministry,
 			lifetime_count: new_lifetime
 		)
-		#redirect_to action: "show", controller: "profile"
+		ministry.members << @user
+		peer_class.members << @user
+
+		redirect_to action: "show", controller: "profile"
 	end
 end
