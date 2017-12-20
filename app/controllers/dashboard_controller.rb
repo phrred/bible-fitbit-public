298c24167131717e173fb277978948dbc3ca4636
Matt Miller <matt.b.miller@gmail.com>
2017-12-20 00:02:59 -0500

    chart fixes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,6 +6,12 @@ class DashboardController < ApplicationController
 		user_id = session[:user_id]
 		@user = User.where(id: user_id).take
 		@all_ministry_names = Group.where(group_type: "ministry").order(:name).pluck(:name)
+		last_read_entry = ReadEvent.where(user_id: session[:user_id]).order("updated_at DESC").first
+		if last_read_entry != nil
+			@last_book_read = last_read_entry.chapter.book
+		else
+			@last_book_read = "Genesis"
+		end
 	end
 	def show
 		config_dashboard()
@@ -15,17 +21,20 @@ class DashboardController < ApplicationController
 		@y_axis_max = 10
 
 		@user_shadowings = UserShadowing.where(user_id: @user)
-		chapters_read = @user_shadowings.count()
+		chapters_read = 0
+		@user_shadowings.each do |shadows|
+			chapters_read += shadows.shadowing.count()
+		end
 		@percentage_of_bible = chapters_read.to_f / bible_chapter_count
 
 		last_read_entry = ReadEvent.where(user_id: session[:user_id]).order("updated_at DESC").first
 		if last_read_entry != nil
 			@last_book_read = last_read_entry.chapter.book
-			last_book_shadowings = @user_shadowings.where(book: @last_book_read)
-			last_book_chapters_read = last_book_shadowings.count()
+			last_book_shadowings = @user_shadowings.where(book: @last_book_read).take
+			last_book_chapters_read = last_book_shadowings.shadowing.count()
 			
 			book_chapter_count = Chapter.where(book: @last_book_read).count()
-			@percentage_of_last_book = last_book_chapters_read.to_f / book_chapter_count
+			@percentage_of_last_book = last_book_chapters_read.to_f / book_chapter_count * 100.0
 		else
 			@percentage_of_last_book = "You need to read the bible"
 		end
@@ -49,35 +58,42 @@ class DashboardController < ApplicationController
 			@your_pace[pace_chart_start + 7],
 			@your_pace[pace_chart_start + 14],
 			@your_pace[pace_chart_start + 21]]
+
+		@initial_repetition_labels = Array.new(@book_repetitions[@last_book_read].size-1){|i| "Chapter " + String(i+1)}
+		@initial_repetition_labels.unshift(@last_book_read)
 	end
 
 	def generate_your_percentile()
 		count_ids = Count.where(year: 0).order(:count).pluck(:id)
 		your_rank = count_ids.index(@user.lifetime_count.id)
-		@your_ranking = your_rank*100/count_ids.size()
-		@next_percentile = (@your_ranking/10+1).floor*10
-		@next_percentile_id = (@next_percentile/100*count_ids.size()).floor
+		@your_ranking_percentile = your_rank*100.0/(count_ids.size() - 1)
+		p("PERCENTILE")
+		p(count_ids)
+		p(your_rank)
+		p(@your_ranking)
+		@next_percentile = @your_ranking_percentile == 100.0 ? 100.0 : (@your_ranking_percentile/10+1).floor*10
+		@next_percentile_id = (@next_percentile/100*(count_ids.size()-1)).floor
 		@next_ten_percent = Count.where(id: count_ids[@next_percentile_id]).pluck(:count)
 	end
 
-	def see_book_percent_read	
+	def see_book_percent_read
 		@book_percentages = {}
 		bible_books.each do |book|
-			chapters_read = @user_shadowings.where(book: book).count()
-			@book_percentages[book] = chapters_read.to_f/Chapter.where(book: book).count()
+			chapters_read = UserShadowing.where(user_id: @user, book: book).take.shadowing.count
+			@book_percentages[book] = chapters_read.to_f/Chapter.where(book: book).count() * 100.0
 		end
 	end
 
 	def how_many_reps
 		@x_axis_max = 0
-		@user_readings = ReadEvent.where(user: @user).group(:chapter)
+		@user_readings = ReadEvent.where(user: @user).group("chapter_id").count
 		@book_repetitions = {}
 		bible_books.each do |book|
 			@book_repetitions[book] = []
 			Chapter.where(book: book).pluck(:id).each do |id|
-				if !@user_reading.nil? and @user_reading.key?id
+				if !@user_readings.nil? and @user_readings.key?id
 					if @user_readings[id] > @x_axis_max
-						@x_axis_max = user_readings[id]
+						@x_axis_max = @user_readings[id]
 					end
 					@book_repetitions[book] << @user_readings[id]
 				else
