e77b5d2ec0d3599ffd5a5be041adcb0be6c95fb1
Matt Miller <matt.b.miller@gmail.com>
2017-12-25 12:17:18 -0500

    working rake

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -309,36 +309,4 @@ class ChallengesController < ApplicationController
 			format.js
 		end
 	end
-  	def check_ending_challenges
-  		if Date.today.sunday?
-			monday = Date.today.beginning_of_week
-			challenges = Challenge.where("start_time = ?", monday)
-			challenges.find_each do |challenge|
-				sender_group = challenge.sender_ministry
-				sender_number = 0
-				receiver_number = 0
-				sender_sum = 0
-				receiver_sum = 0
-				entries = ChallengeReadEntry.where(challenge: challenge, accepted: true)
-				if entries != nil
-					entries.each do |entry|
-						if is_user_in_group(entry.user, sender_group)
-							sender_number = sender_number + 1
-							sender_sum = sender_sum + entry[:chapters].size
-						else
-							receiver_number = receiver_number + 1
-							receiver_sum = receiver_sum + entry[:chapters].size
-						end
-					end
-				end
-				sender_final = sender_number != 0 ? sender_sum/sender_number.to_f : 0
-				receiver_final = receiver_number != 0 ? receiver_sum/receiver_number.to_f : 0
-				if sender_final > receiver_final
-					challenge.update(winner: true)
-				else
-					challenge.update(winner: false)
-				end
-			end
-		end
-  	end
 end
