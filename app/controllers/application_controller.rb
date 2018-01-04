531427fec56c7f26e8471d3df03b855138247b9a
Matt Miller <matt.b.miller@gmail.com>
2018-01-03 16:39:55 -0800

    im not sure what I changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,6 +25,10 @@ class ApplicationController < ActionController::Base
   end
 
   def verify_oath_user
+    if request.user_agent =~ /Mobile|webOS/
+      redirect_to mobile_path
+      return
+    end
     if not current_oath_user()
       redirect_to controller: 'login', action: 'show'
     end
