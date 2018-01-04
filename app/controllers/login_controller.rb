22434fa4887b428c8d4d77a6acccde3706af5fd3
Matt Miller <matt.b.miller@gmail.com>
2018-01-03 16:41:01 -0800

    never do it right the first time

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,7 +2,6 @@ class LoginController < ApplicationController
 	skip_before_action :verify_user, only: [:show]
 	skip_before_action :verify_oath_user, only: [:show]
 	def show
-		redirect_to mobile_path
 		if session[:user_id] != nil
 			redirect_to dashboard_path
 		end
